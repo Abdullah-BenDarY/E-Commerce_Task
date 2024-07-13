f0ab7ed7c694bd08e5a22eabeb030ea3808d77ca
AbdullahBendary <abdullahbendary7@gmail.com>
2024-07-14 00:34:10 +0300

    Create products fragment

---
+++ app/src/main/java/com/example/e_commercetask/ui/ProductsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.e_commercetask.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.e_commercetask.R
+import com.example.e_commercetask.base.BaseFragment
+import com.example.e_commercetask.databinding.FragmentProductsBinding
+
+
+class ProductsFragment : BaseFragment<FragmentProductsBinding>(FragmentProductsBinding::inflate){
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
