45abd02888151869bb8f80d54d2a9de7f5fc0699
AbdullahBendary <abdullahbendary7@gmail.com>
2024-07-14 14:47:29 +0300

    Implement product view model

---
+++ app/src/main/java/com/example/e_commercetask/ui/ProductsViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.e_commercetask.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.e_commercetask.pojo.Product
+import com.example.e_commercetask.repo.Repository
+import com.example.e_commercetask.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductsViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+
+    private var _productsLiveData = MutableLiveData<Resource<Product>>()
+    val productsLiveData get() = _productsLiveData
+    private var saveStateProducts: Resource<Product>?=null
+
+
+    fun getMealsDetails() {
+        saveStateProducts?.let {
+            productsLiveData.postValue(it)
+            return
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = repository.getProducts()
+                if (response.products.isNotEmpty()) {
+                    _productsLiveData.postValue(Resource.Success(response.products[0]))
+                    saveStateProducts = Resource.Success(response.products[0])
+
+                } else {
+                    _productsLiveData.postValue(Resource.Error(response.toString()))
+                }
+
+            } catch (e: Exception) {
+                _productsLiveData.postValue(Resource.Error("An error occurred: ${e.message}"))
+            }
+        }
+    }
+}
